a6b0f74a73ffb755d5e79214f40aee4060318c3a
HSRAKTU <rockpriyanshu3@gmail.com>
2024-02-01 02:03:14 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import { express } from "express";
+import  express  from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
@@ -10,11 +10,16 @@ app.use(cors({
 }))
 
 app.use(express.json({limit: "16kb"}))
-
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
-
 app.use(express.static("public"))
-
 app.use(cookieParser())
+
+//routes import
+
+import userRouter from "./routes/user.routes.js";
+
+app.use("/api/v1/users", userRouter);
+
+//http://localhost:8000/api/v1/users/register
 
 export { app }
